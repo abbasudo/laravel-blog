be075b118d528f4e12a088ada8fadb88631133ac
Abbas mkhzomi <amkhzomi@gmail.com>
2024-05-13 18:53:45 +0330

    session 1

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', function (Request $request){
-    return ['laravel'];
+Route::post('/register', function (Request $request) {
+    return Response::json($request->all())->setStatusCode(201);
 });
