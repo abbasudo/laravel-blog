584579c5d18079382710aeab79c53a68283e9d8b
Abbas mkhzomi <amkhzomi@gmail.com>
2024-05-17 12:32:04 +0330

    session 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +12,18 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', function (Request $request) {
     return Response::json($request->all())->setStatusCode(201);
+});
+
+Route::get('/posts', function (Request $request) {
+//    $data = DB::table('posts')
+//        ->where('likes', '!=' ,3)
+//        ->orWhere('published_at', null)
+//        ->get();
+
+    $startDate = Carbon::createFromDate(2024, 2, 1);
+    $endDate =  Carbon::createFromDate(2025, 6, 1);
+
+    $data = DB::table('posts')->whereNotBetween('published_at', [$startDate, $endDate])->get();
+
+    return Response::json($data)->setStatusCode(200);
 });
