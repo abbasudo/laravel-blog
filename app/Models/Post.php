0e4ebd3f8a567dc53aee64d10fae416d1f4e30fa
Abbas mkhzomi <amkhzomi@gmail.com>
2024-05-18 13:41:22 +0330

    session 3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'published_at',
+        'description',
+        'image_url',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +15,25 @@ Route::post('/register', function (Request $request) {
 });
 
 Route::get('/posts', function (Request $request) {
+    dd(Post::find(1));
 
     $startDate = Carbon::createFromDate($request->input('start_date'))->getTimestampMs();
 
-    $endDate =  Carbon::createFromDate($request->input('end_date'))->getTimestampMs();
+    $endDate = Carbon::createFromDate($request->input('end_date'))->getTimestampMs();
 
-    $data = DB::table('posts')->whereBetween('published_at', [$startDate, $endDate])->get();
+    $posts = Post::whereBetween('published_at', [$startDate, $endDate])->get();
 
-    return Response::json($data)->setStatusCode(200);
+    return Response::json($posts)->setStatusCode(200);
+});
+
+
+Route::post('/posts', function (Request $request) {
+    $post = Post::create([
+        'title'        => $request->get('title'),
+        'description'  => $request->get('description'),
+        'image_url'    => $request->get('image_url'),
+        'published_at' => $request->get('published_at'),
+    ]);
+
+    return Response::json($post)->setStatusCode(200);
 });
